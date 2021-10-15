6794443a76dac69954645f4392426700520e0af1
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-10-15 19:32:23 +0530

    Added: Cosine angle Program is Added

---
+++ Cosine.java
@@ -0,0 +1,17 @@
+public class Cosine {
+
+    public static void main(String args[])
+        {
+
+            double degrees = 60.0;
+
+            double radians = Math.toRadians(degrees);
+
+            double cosValue = Math.cos(radians);
+
+            System.out.println("cos(" + degrees + ") = " + cosValue);
+
+        }
+    }
+
+
